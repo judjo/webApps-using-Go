cf5c822dec4469bc2bbd7782d3b022457d6f5efe
docmobile17 <docmantapmobile@gmail.com>
2020-04-20 01:00:57 +0700

    update code
    update code all

---
+++ src/main/02web.go
@@ -0,0 +1,31 @@
+package main
+
+import(
+	"net/http"
+	"github.com/gorilla/mux"
+)
+
+func main(){
+	serveWeb()
+}
+
+func serveWeb(){
+
+	gorillaRoute := mux.NewRouter()
+
+	gorillaRoute.HandleFunc("/", serveContent)
+	gorillaRoute.HandleFunc("/{page_alias}", serveContent)
+
+	http.Handle("/", gorillaRoute)
+	http.ListenAndServe(":8080", nil)
+}
+
+
+func serveContent(w http.ResponseWriter, r *http.Request){
+	urlParams := mux.Vars(r)
+	page_alias := urlParams["page_alias"]
+	if page_alias == ""{
+		page_alias = "home"
+	}
+	w.Write([]byte("Hello World!" + page_alias))
+}
